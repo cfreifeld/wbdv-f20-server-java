8778e94ec60e014979ef3a45015618ab1debe367
Clark Freifeld <ccf@ccs.neu.edu>
2020-09-25 09:42:55 -0400

    alternate impl for emptying tbody: stash template in a variable; fix up remove-by-id

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -18,31 +18,34 @@ let users = [
     role: 'ADMIN'
   }
 ]
-
+let $template
 const init = () => {
   //jQuery('h1').css('backgroundColor', 'blue')
   //let $ul = $('<ul>')
   //for(let i = 0; i < users.length; i++) {
+  $template = $('.wbdv-template').clone()
   $('.wbdv-create').click(() => {
     console.log($('#usernameFld').val())
     $('#usernameFld').val('')
   })
+  // TODO: Use the findAllUsers function instead
   $.get('https://wbdv-generic-server.herokuapp.com/api/jannunzi/users',
      renderUsers)
 }
-
 function renderUsers(users) {
   //var $newRow = $userRowTemplate.clone(withDataAndEvents=true);
-  $('tr.wbdv-added').remove();
+  //$('tr.wbdv-added').remove();
+  let $tbody = $('tbody')        // as suggested by IntelliJ: find tbody once and store it
+  $tbody.empty()
   for (let u of users) {
     //$ul.append($('<li>' + u.username + '</li>'));
-    let $newRow = $('.wbdv-template').clone()
+    let $newRow = $template.clone()
     //let $newRow = $userRowTemplate.clone()
-    $newRow.removeClass('wbdv-template').removeClass('wbdv-hidden').addClass('wbdv-added')
-    $newRow.attr('wbdv-username', u._id)
+    $newRow.removeClass('wbdv-template').removeClass('wbdv-hidden')//.addClass('wbdv-added')
+    $newRow.attr('wbdv-userid', u._id)
     $newRow.find('.wbdv-username').html(u.username)
     $newRow.find('.wbdv-first-name').html(u.first)
-    $('tbody').append($newRow)
+    $tbody.append($newRow)
   }
   //$('div.container').append($ul)
   $('.wbdv-remove').click((evt) => deleteUser2(evt, users))
@@ -50,9 +53,11 @@ function renderUsers(users) {
 
 function deleteUser2(evt, users) {
   // determine which user was clicked
-  const uname = $(evt.currentTarget).parents('tr.wbdv-user').attr('wbdv-username')
+  const id = $(evt.currentTarget).parents('tr.wbdv-user').attr('wbdv-userid')
   // remove that user from array
-  users = users.filter(u => u.username !== uname);
+  users = users.filter(u => u._id !== id);
+  // TODO: instead of removing user from the local array, make DELETE request to
+  // the server, and then findAllUsers to get the updated list of users.
   renderUsers(users)
 }
 
